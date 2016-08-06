b7f5c9fb01cbf4f819d60564f39f4ce9a56bb940
kingwen0302 <kingwen0302@msn.com>
2016-08-06 17:05:45 +0800

    fix install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,13 @@
 @echo off
 
-CUR_DIR=.
+set CUR_DIR=%~dp0
 mkdir %CUR_DIR%\bundle
 
 git clone https://github.com/VundleVim/Vundle.vim.git %CUR_DIR%\bundle\Vundle.vim
 
-echo "run:"
-echo "let g:my_vimrc_dir = %CUR_DIR%"
-echo 'exe "source " .  g:my_vimrc_dir . "/vimrc/myvimrc"'
-echo 'vim -c ":BundleInstall" -c ":exit" -c ":exit"'
+echo 1. _vimrc修改如下:
+echo let g:my_vimrc_dir = %CUR_DIR%
+echo exe "source " .  g:my_vimrc_dir . "/vimrc/myvimrc"
+echo 2. 执行
+echo vim -c ":BundleInstall" -c ":exit" -c ":exit"
+
